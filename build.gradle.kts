d3cf4883b884a3d401abdd64b488c54fcb86c9ea
JDDev0 <jddev0@dorneger.at>
2026-01-17 11:23:38 +0100

    Change version to 0.1.1-dev

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 }
 
 group = "me.jddev0"
-version = "0.1.0"
+version = "0.1.1-dev"
 val javaVersion = 25
 
 val appData = System.getenv("APPDATA") ?: (System.getenv("HOME") + "/.var/app/com.hypixel.HytaleLauncher/data")
