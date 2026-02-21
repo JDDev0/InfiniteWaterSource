517edd268b383126f8f047f0939bf0770e18a84e
JDDev0 <jddev0@dorneger.at>
2026-02-21 22:10:48 +0100

    Update Hytale Server version to 2026.02.19-1a311a592 and change version to 0.1.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 }
 
 group = "me.jddev0"
-version = "0.1.2"
+version = "0.1.3"
 val javaVersion = 25
 
 val appData = System.getenv("APPDATA") ?: (System.getenv("HOME") + "/.var/app/com.hypixel.HytaleLauncher/data")
